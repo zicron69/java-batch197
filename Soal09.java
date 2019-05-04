eb2cc6e39a8fd7a59cbaac19c304c69dbdadff64
zicron69 <47386651+zicron69@users.noreply.github.com>
2019-05-04 09:27:31 +0700

    Add files via upload

---
+++ Soal09.java
@@ -0,0 +1,25 @@
+package day01;
+import java.util.Scanner;
+
+public class Soal09 {
+	public static Scanner input;
+	
+	public static void main(String[] args) {
+		input = new Scanner(System.in);
+		System.out.println("Input Your Number : ");
+		
+		int n = Integer.parseInt(input.nextLine());
+		int number=4;
+		
+		for (int i = 1; i <= n; i++) {
+			if (i % 3 == 0) {
+				System.out.println("*");
+			} else {
+				System.out.println(number);
+				number=number*4;
+			} 
+		}
+
+	}
+
+}
